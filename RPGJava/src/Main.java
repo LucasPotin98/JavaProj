aee50c4c84b53708c72b8fa70fc8749c097986cf
mamasse76 <44226227+mamasse76@users.noreply.github.com>
2018-10-30 09:38:20 +0100

    Github c'est de la merde
    j'ai été obligé de fouttre ça dans le main je sais pas pourquoi, du coup c'est la version 1 de la map la deuxième passait pas sur git faut que j'essaie de la mettre mais c'est le même principe avec une carte bcp plus grande et avec des décors+changement de carte quand on arrive au bout d'une.

--- RPGJava/src/Main.java
+++ RPGJava/src/Main.java
@@ -1,9 +1,74 @@
 
-public class Main {
+package mapping;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello World");
-	}
 
+public class main{
+	// ici on crée la map
+    public static void main(String[] args){
+        // Création de la map
+        int[][] map = new int[10][10];
+        for(int i = 0; i < map.length; i++){
+            map[i] = new int[10];
+        }
+        
+       // on initialise la position du perso 
+       int pospersonnageX =0 ;
+       int pospersonnageY =0 ;
+       
+        map[pospersonnageX][pospersonnageY]=1;
+        
+        char inp = 'z';
+     // Affichage de la map
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[i].length; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        // on bouge avec zqsd
+        while(inp=='z'|| inp=='q'|| inp=='s'|| inp=='d') {
+        	
+        
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choisir une direction");
+        String str=sc.nextLine();
+        inp= str.charAt(0);
+        if(inp=='d') {
+        	map[pospersonnageX][pospersonnageY]=0;
+        	if (pospersonnageY<map[0].length-1) {
+        	pospersonnageY++;
+        	}
+        }
+        if(inp=='q') {
+        	map[pospersonnageX][pospersonnageY]=0;
+        	if (pospersonnageY>0) {
+        	pospersonnageY--;
+        	}
+        }
+        if(inp=='z') {
+        	map[pospersonnageX][pospersonnageY]=0;
+        	if (pospersonnageX>0) {
+        	pospersonnageX--;
+        	}
+        }
+        if(inp=='s') {
+        	map[pospersonnageX][pospersonnageY]=0;
+        	if (pospersonnageX<map.length-1) {
+        	pospersonnageX++;
+        	}
+        }
+        map[pospersonnageX][pospersonnageY]=1;
+        // Affichage la map à chaque mouvement
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[i].length; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    }
 }
